3717352ab4290f01d48b2dea67331f69d512dd4c
Eldo Magan <magan.eldo@gmail.com>
2025-05-03 21:56:53 +0100

    fix: merge RichText & InlineRichText setting types

--- src/Sections/Settings/InlineRichText.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace BagistoPlus\Visual\Sections\Settings;
-
-class InlineRichText extends Base
-{
-    public static string $component = 'inline-richtext-setting';
-}
--- src/Sections/Settings/RichText.php
+++ src/Sections/Settings/RichText.php
@@ -5,4 +5,21 @@ namespace BagistoPlus\Visual\Sections\Settings;
 class RichText extends Base
 {
     public static string $component = 'richtext-setting';
+
+    public bool $inline = false;
+
+    public function inline(): self
+    {
+        $this->inline = true;
+
+        return $this;
+    }
+
+    public function toArray(): array
+    {
+        return array_merge(parent::toArray(), [
+            'type' => $this->inline ? 'inline_richtext' : 'richtext',
+            'inline' => $this->inline,
+        ]);
+    }
 }
