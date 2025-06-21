237b0f37d1fdc3e7f1cac24d7ced990529d30269
Eldo Magan <magan.eldo@gmail.com>
2025-06-21 13:23:04 +0100

    feat(docs): add video to homepage hero section

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,29 @@
+<script setup>
+  import DefaultTheme from "vitepress/theme";
+</script>
+
+<template>
+  <DefaultTheme.Layout>
+    <template #home-hero-image>
+      <iframe
+        class="video"
+        src="https://www.youtube.com/embed/fB7ik1hOWMc?autoplay=1&mute=1&loop=1&playlist=fB7ik1hOWMc&start=7&modestbranding=1&showinfo=0&rel=0"
+        title="Introducing Bagisto Visual – A New Way to Build Bagisto Stores Visually"
+        frameborder="0"
+        allow="autoplay; encrypted-media"
+        allowfullscreen
+      ></iframe>
+    </template>
+  </DefaultTheme.Layout>
+</template>
+
+<style scoped>
+.video {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 100%;
+  aspect-ratio: 16/9;
+  transform: translate(-50%, -50%);
+}
+</style>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,9 +4,11 @@ import { Tab, Tabs } from 'vue3-tabs-component';
 import '@red-asuka/vitepress-plugin-tabs/dist/style.css';
 import './custom.css';
 import SettingPreview from './components/SettingPreview.vue';
+import Layout from './Layout.vue';
 
 export default {
   extends: DefaultTheme,
+  Layout,
   enhanceApp({ app }) {
     app.component('Tab', Tab);
     app.component('Tabs', Tabs);
