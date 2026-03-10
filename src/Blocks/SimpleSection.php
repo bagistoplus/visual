0007a28b1bb1f4a527dff1b8ee854721ef48817a
Eldo Magan <magan.eldo@gmail.com>
2026-03-10 04:50:17 +0100

    feat: add section alias for block in SimpleSection

--- src/Blocks/SimpleSection.php
+++ src/Blocks/SimpleSection.php
@@ -4,6 +4,15 @@ namespace BagistoPlus\Visual\Blocks;
 
 class SimpleSection extends SimpleBlock
 {
+    public function __get($name)
+    {
+        if ($name === 'section') {
+            return $this->block;
+        }
+
+        return null;
+    }
+
     public function data()
     {
         return [
